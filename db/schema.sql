17a150cd91ef72e5e7cf8a90df71c85d6ded0ae2
Pat Perry <risingphx1@gmail.com>
2019-12-22 09:30:24 -0500

    sql files created

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,14 @@
+DROP DATABASE IF EXISTS getburger_DB;
+
 -- Create the database burgers_db.
-CREATE DATABASE burgers_db;
+CREATE DATABASE getburger_db;
 USE getburger_db;
 
 -- Create the table burgers.
 CREATE TABLE burgers
 (
     id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(50) NOT NULL,
+    burger VARCHAR(50) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Creating seed data for database
+INSERT INTO burgers (burger) VALUES ("The Dallas Cowboy Burger", false);
+INSERT INTO burgers (burger) VALUES ("Da Mac & Cheese Burger", false);
+INSERT INTO burgers (burger) VALUES ("Peppery Pepper Jack Burger", false);
+INSERT INTO burgers (burger) VALUES ("Mushroom Onion Burger", false)
